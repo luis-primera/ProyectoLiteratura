0b6c6e4e89ff7ae6c9689fe54eac4627cee2e785
luis-primera <luisepb24@gmail.com>
2025-07-10 21:34:19 -0400

    se carga proyecto

---
+++ src/test/java/com/example/ProyectoLiteratura/ProyectoLiteraturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoLiteratura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoLiteraturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
